b0d7b47eaf1b433723d8e3716e8c5878c35e6584
kiseong.park <kiseong.park@stunitas.com>
2019-12-17 22:40:45 +0900

    temporary save

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-      <player></player>
+      <player
+
+      ></player>
   </div>
 </template>
 
--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,18 +1,44 @@
 <template>
-    <div class="player-body">
+    <div
+        class="player-body"
+        :class="{paused}"
+    >
         <div class="video-container" :style="{width, height}">
-            <video ref="video" style="background: #333;">
-                <source src="./file_example_MP4_480_1_5MG.mp4" type="video/mp4">
+            <video
+                ref="video"
+                @abort="abort"
+                @canplay="canplay"
+                @canplaythrough="canplaythrough"
+                @durationchange="durationchange"
+                @emptied="emptied"
+                @ended="ended"
+                @error="error"
+                @loadeddata="loadeddata"
+                @loadedmetadata="loadedmetadata"
+                @loadstart="loadstart"
+                @pause="pause"
+                @play="play"
+                @playing="playing"
+                @progress="progress"
+                @ratechange="ratechange"
+                @seeked="seeked"
+                @seeking="seeking"
+                @stalled="stalled"
+                @suspend="suspend"
+                @timeupdate="timeupdate"
+                @volumechange="volumechange"
+                @waiting="waiting"
+            >
             </video>
             <div class="video-controls">
                 <div style="position: absolute; width:100%; top:0px; bottom:0px; box-sizing: border-box; " @click="playPause">
 
                 </div>
-                <div style="position: absolute; width:100%; height: 100px; box-sizing: border-box; background:rgba(0, 0, 255, 0.2);">
+                <div class="video-title" style="position: absolute; width:100%; height: 60px; box-sizing: border-box; background:rgba(0, 0, 255, 0.2);">
 
                 </div>
-                <div style="position: absolute; bottom: 0; width:100%; height: 100px; box-sizing: border-box; background:rgba(255, 0, 0, 0.2);">
-                    <button class="play" :class="{paused}" @click="playPause"></button>
+                <div class="control-panel" style="position: absolute; bottom: 0; width:100%; height: 60px; box-sizing: border-box; background:rgba(255, 0, 0, 0.2);">
+                    <button class="play" @click="playPause"></button>
                 </div>
             </div>
         </div>
@@ -20,7 +46,8 @@
     </div>
 </template>
 <script>
-        export default {
+    import Hls from 'hls.js'
+    export default {
         name: 'Player',
         props: {
             width: {
@@ -34,26 +61,120 @@
         },
         data: function () {
             return {
-                paused: false
+                playPromise: null,
+                paused: true,
             }
         },
         mounted: function () {
-
+            let video = this.$refs.video
+            if (Hls.isSupported()) {
+                let hls = new Hls()
+                hls.loadSource('https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8')
+                hls.attachMedia(video)
+                hls.on(Hls.Events.MANIFEST_PARSED,function () {
+                    video.play().catch(() => {
+                        video.muted = true
+                        video.play()
+                    })
+                })
+            } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+                video.src = 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8';
+                video.addEventListener('canplay', function () {
+                    video.play().catch(() => {
+                        video.muted=true
+                        video.play()
+                    })
+                })
+            }
         },
         methods: {
             playPause: function() {
-
-                this.paused = !this.paused
                 if (this.paused) {
-                    this.$refs.video.play()
+                    this.playPromise = this.$refs.video.play()
                 } else {
-                    this.$refs.video.pause()
+                    if (this.playPromise) {
+                        this.playPromise.then(() => {
+                            this.$refs.video.pause()
+                        }).catch(() => {
+                            this.$refs.video.pause()
+                        })
+                    } else {
+                        this.$refs.video.pause()
+                    }
                 }
+            },
 
-            }
+            abort: function () {
+                console.log('abort')
+            },
+            canplay: function () {
+                console.log('canplay')
+            },
+            canplaythrough: function () {
+                console.log('canplaythrough')
+            },
+            durationchange: function () {
+                console.log('durationchange')
+            },
+            emptied: function () {
+                console.log('emptied')
+            },
+            ended: function () {
+                console.log('ended')
+            },
+            error: function () {
+                console.log('error')
+            },
+            loadeddata: function () {
+                console.log('loadeddata')
+            },
+            loadedmetadata: function () {
+                console.log('loadedmetadata')
+            },
+            loadstart: function () {
+                console.log('loadstart')
+            },
+            pause: function () {
+                this.paused = true
+                console.log('pause')
+            },
+            play: function () {
+                console.log('play')
+                this.paused = false
+            },
+            playing: function () {
+                console.log('playing')
+            },
+            progress: function () {
+                console.log('progress')
+            },
+            ratechange: function () {
+                console.log('ratechange')
+            },
+            seeked: function () {
+                console.log('seeked')
+            },
+            seeking: function () {
+                console.log('seeking')
+            },
+            stalled: function () {
+                console.log('stalled')
+            },
+            suspend: function () {
+                console.log('suspend')
+            },
+            timeupdate: function () {
+                console.log('timeupdate')
+            },
+            volumechange: function () {
+                console.log('volumechange')
+            },
+            waiting: function () {
+                console.log('waiting')
+            },
         }
     }
-</script>
+    </script>
 <style scoped>
     .player-body {
         -webkit-touch-callout: none;
@@ -67,7 +188,7 @@
         position: relative;
         width: 100%;
         height: 100%;
-        background-color: black;
+        /* background-color: black; */
         display: -webkit-box;
         display: -moz-box;
         display: -ms-flexbox;
@@ -96,7 +217,6 @@
         left: 0;
         position: absolute;
     }
-
     .play {
         margin: 10px;
         border: 0;
@@ -107,12 +227,23 @@
         border-color: transparent transparent transparent #ffffff;
         transition: 200ms all ease;
         cursor: pointer;
+        border-style: double;
+        border-width: 0 0 0 18.3px;
+    }
+    .paused .play {
         border-style: solid;
         border-width: 10px 0 10px 18.3px;
     }
-    .play.paused {
-        border-style: double;
-        border-width: 0px 0 0px 18.3px;
+    .control-panel {
+        display: none;
     }
-
+    .video-title {
+        display: none;
+    }
+    .video-controls:hover .control-panel {
+        display: block;
+    }
+    .paused .control-panel,.paused .video-title {
+        display: block;
+    }
 </style>
